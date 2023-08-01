3a47df3d8d95f3cdc54e5bc5ac681ac25e35ba74
ranjan7707 <ranjan7707@gmail.com>
2023-08-01 20:05:15 +0530

    candy project

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const Candy = require('./models/Candy.js')
 const cors = require('cors')
 
+const ejs = require('ejs');
+
 // const errorController = require('./controllers/error');
 const sequelize =require('./util/database')
 
@@ -12,8 +14,10 @@ const app = express();
 
 app.use(cors())
 
-app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 app.set('views', 'views');
+app.set('views', __dirname + '/views');
 const adminRoutes  = require('./routes/candies');
 
 app.use(bodyParser.json({ extended: false }));
@@ -21,8 +25,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(adminRoutes)
 
-sequelize.sync().then(res=>{
-    // console.log(res)
-    app.listen(3000);
-})
-.catch(e=>console.log(e))
+// Route to render the candy_shop.html using EJS
+app.get('/shop', (req, res) => {
+    console.log(req)
+    res.render('candy_shop');
+  });
+
+// Sync the model with the database (create the Candy table if it doesn't exist)
+sequelize
+  .sync()
+  .then(() => {
+    console.log('Connected to the database and synced the models.');
+    app.listen(3000, () => {
+      console.log('Server started and listening on port 3000.');
+    });
+  })
+  .catch((err) => {
+    console.error('Error connecting to the database:', err.message);
+  });
+
+// sequelize.sync().then(res=>{
+//     console.log(res)
+//     app.listen(4000);
+// })
+// .catch(e=>console.log(e))
+
--- util/database.js
+++ util/database.js
@@ -4,3 +4,4 @@ const sequelize = new Sequelize('node-complete','root','HDFCrr@7707',
 {dialect:'mysql',host:'localhost'})
 
  module.exports=sequelize
+
